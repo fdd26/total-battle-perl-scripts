dd96761025aaafc0d057be106be30ab24bcce2db
fdd26 <166243616+fdd26@users.noreply.github.com>
2024-10-20 04:37:36 -0400

    implement tb-mouseclick-150x.pl

--- tb-mouseclick-150x.pl
+++ tb-mouseclick-150x.pl
@@ -54,188 +54,116 @@ use Data::Dumper;
 #########################################################################################
 #
 #
-# PASTE your Excel TSV columns between the multiline EOF below...
+# Total Battle - Daily Job Free Mouse Click repeat x 150 every other seconds
 #
+#use strict;
+no strict;
 #
-#########################################################################################
+use warnings;
+use v5.14;
 
-my $points_tsv=<<EOF;
+use Win32::Sound;
 
+#Win32::Sound::Play("SystemStart");
 
-EOF
-;
+srand(time());
 
-my %countHash=();
+$| = 1;
 
 #
-# Point HashMap
+# Mouse 32-bit API
 #
-my %pointHash = (
+use Win32::API;
 
-	# Normal Crypt
-	'c25'  => 19,
-	'c20'  =>  7,
-	'c15'  =>  3,
-	'c10'  =>  0,
-	'c5'   =>  0,
+#
+# @see https://stackoverflow.com/questions/896904/how-do-i-sleep-for-a-millisecond-in-perl
+#
+use Time::HiRes qw(usleep);
 
-	# Rare Crypt
-	'rc35' => 126,
-	'rc30' => 70,
-	'rc25' => 32,
-	'rc20' => 17,
-	'rc15' => 10,
-	'rc10' =>  0,
-	'rc5'  =>  0,
+#
+# @see https://metacpan.org/release/BULKDD/Win32-API-0.84/source/samples/GetCursorPos.pl
+#
+Win32::API::Struct->typedef( 'POINT' => qw( LONG x; LONG y; ) );
+Win32::API->Import('user32' => 'BOOL GetCursorPos(LPPOINT pt)');
+Win32::API->Import('user32' => 'BOOL SetCursorPos(int x, int y)');
+Win32::API->Import('user32' => 'void mouse_event(int dwFlags, int dx, int dy, int dwData, int dwExtraInfo)');
 
-	# Epic Crypt
-	'ec35' => 126,
-	'ec30' => 60,
-	'ec25' => 32,
-	'ec20' => 17,
-	'ec15' => 10,
-	'ec10' =>  0,
-	'ec5'  =>  0,
-
-	# Citadel
-	'k35'   =>  60,
-	'k30'   =>  60,
-	'k25'   =>  24,
-	'k20'   =>  11,
-	'k15'   =>  4,
-	'k10'   =>  0,
-
-	# Citadel
-	'cit35' =>  60,
-	'cit30' =>  60,
-	'cit25' =>  24,
-	'cit20' =>  11,
-	'cit15' =>  4,
-	'cit10' =>  0,
-
-	# Cursed Citadel
-	'cursed35' =>  60,
-	'cursed30' =>  60,
-	'cursed25' =>  24,
-	'cursed20' =>  11,
-	'cursed15' =>  4,
-	'cursed10' =>  0,
-
-	#HERO
-	'heroic17' => 11,
-
-	''     =>  0
-);
-
-sub parseItems()
+sub getMouseXYCoordinates()
 {
-	# Split by lines
-	my @list = split /[\r\n]+/, $points_tsv;
-
-	my $points = 0;
-
-	# Loop over all lines
-	foreach my $item ( @list )
-	{
-		chomp $item;
-
-		# Trim spaces
-		$item =~ s/^\s+|\s+$//gm;
-
-		# Match item lines by known hash key prefix
-		if (($item =~ m/^\s*([rcek]+i?t?a?d?e?l?|cursed)\s*([123450]+)\s*/mi)
-		||  ($item =~ m/^\s*(hero?i?c?)\s*([0-9]+)\s*/mi))
-		{
-			# This will group match either RegExp above that first matches in order
-			my $type = $1;
-			my $val  = $2;
-
-			# Strip citadel key
-			$type =~ s/citad?e?l?\s*$/cit/gm;
-
-			# Reconstitute hash key after stripping
-			my $key  = "$type$val";
-
-			# Must be level 5 to 45 inclusive
-			if (($val >= 5) && ($val <= 45))
-			{
-				print "[$key]:[$item]\n";
-				if (!defined($countHash{$key}))
-				{
-					$countHash{$key} = 1;
-				}
-				else
-				{
-					$countHash{$key}++;
-				}
-
-				my $pt = $pointHash{$key};
-				if (defined($pt))
-				{
-					$points += $pt;
-				}
-			}
-			else
-			{
-				print "SKIP[$item]\n";
-			}
-		}
-		# Strip comment line
-		elsif ($item =~ m/^\s*([xyhH\@]+)([0-9]+)\s*/mi)
-		{
-			print "KNOWN INVALID[$item]\n";
-		}
-		elsif ($item eq "")
-		{
-			#print "EMPTY[$item]\n";
-		}
-		else
-		{
-			print "BAD INVALID[$item]\n";
-		}
-	}
-
-	print Dumper \%countHash;
-	print "\n[$points]\n";
-
-	return \%countHash;
+	#### using Win32 OO semantics
+	my $pt = Win32::API::Struct->new('POINT');
+	GetCursorPos($pt)  or die("GetCursorPos failed: " . $^E);
+	print "Cursor is at: ". $pt->{'x'} .", ". $pt->{'y'} ."\n";
+	return $pt;
 }
 
-sub countPoints()
+my $MOUSEEVENTF_LEFTDOWN  = 0x02;
+my $MOUSEEVENTF_LEFTUP    = 0x04;
+my $MOUSEEVENTF_RIGHTDOWN = 0x08;
+my $MOUSEEVENTF_RIGHTUP   = 0x10;
+
+sub sendMouseRightClick($$)
 {
-	my $points2 = 0;
-	foreach my $k (sort keys %countHash)
-	{
-		my $nb2 = $countHash{$k};
-		my $pt2 = $pointHash{$k};
-
-		if (!defined($nb2)) { $nb2 = 0; }
-		if (!defined($pt2)) { $pt2 = 0; }
-
-		my $t2  = (0+$nb2) * (0+$pt2);
-		print "[$k] : [$nb2] * [$pt2] = [$t2]\n";
-		$points2 += $t2;
-	}
-
-	print "points2 : [$points2]\n";
-
-	return \%countHash;
+	my ($mx, $my) = @_;
+	my $event = 0x18; # $MOUSEEVENTF_RIGHTDOWN | $MOUSEEVENTF_RIGHTUP;
+	mouse_event($event, $mx, $my, 0, 0);
 }
 
-sub main()
+sub sendMouseLeftClick($$)
 {
-	parseItems();
-	countPoints();
+	my ($mx, $my) = @_;
+	my $event = 0x06; # $MOUSEEVENTF_LEFTDOWN | $MOUSEEVENTF_LEFTUP;
+	mouse_event(0x06, $mx, $my, 0, 0);
 }
 
-#########################################################################################
-#
-# Perl script Main / Exit entry points
-#
-#########################################################################################
+my $pt = getMouseXYCoordinates();
 
-main();
-exit();
+my $x = 0;
+my $y = 0;
+
+if (0)
+{
+	$x = 987;
+	$y = 378;
+}
+
+print "MOVE Mouse!\n\n";
+
+if ($x > 0 && $y > 0)
+{
+	SetCursorPos($x, $y);
+}
+
+#exit;
+print "Sleep 5 seconds... GO!\n\n";
+
+
+#Win32::Sound::Play("SystemStart");
+sleep(5);
+
+print "MOVE AND CLICK!\n\n";
+
+if ($x > 0 && $y > 0)
+{
+	SetCursorPos($x, $y);
+}
+
+Win32::Sound::Play("SystemStart");
+
+# Send 150 mouse clicks delayed by 200 ms + randomized 100 ms
+my $max = 150;
+
+for(my $i = 1; $i <= $max; ++$i)
+{
+	sendMouseLeftClick($x, $y);
+	print "[$i]\n";
+	usleep(200000); # 200 ms
+	usleep(int(rand(100000))); # up to 100 ms
+}
+
+Win32::Sound::Play("SystemStart");
+
+exit(0);
 1;
 
 #########################################################################################
